b8c157aea305b2b1ea9ca421d75201b018bd5d50
mograby3500 <mograby3500@gmail.com>
2022-03-28 23:31:54 +0200

    add watchlist

--- auctions/models.py
+++ auctions/models.py
@@ -13,7 +13,7 @@ class Listing(models.Model):
     description = models.CharField(max_length=140)
     price = models.IntegerField()
     watchers = models.ManyToManyField(User, blank=True, related_name="watchlist")
-    image_url = models.URLField(default="https://media.istockphoto.com/vectors/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-vector-id1128826884?k=20&m=1128826884&s=612x612&w=0&h=3GMtsYpW6jmRY9L47CwA-Ou0yYIc5BXRQZmcc81MT78=")
+    image_url = models.URLField()
 
     def __str__(self):
         return f"{self.title} ({self.id})"
--- auctions/views.py
+++ auctions/views.py
@@ -97,8 +97,11 @@ def new_listing(request):
     title = request.POST["title"]
     description = request.POST["description"]
     price = request.POST["price"]
-    image_url = request.POST["image_url"]
-
+    if request.POST["image_url"] == "":
+        image_url = "https://media.istockphoto.com/vectors/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-vector-id1128826884?k=20&m=1128826884&s=612x612&w=0&h=3GMtsYpW6jmRY9L47CwA-Ou0yYIc5BXRQZmcc81MT78="
+    else:
+        image_url = request.POST["image_url"]
+    
     listing = Listing(
         user=request.user, 
         title=title, 
@@ -125,3 +128,12 @@ def listing(request, listing_id):
         new_comment = Comment(user=user, content= content, listing=Listing.objects.get(pk=listing_id))
         new_comment.save()
         return HttpResponseRedirect(reverse("listing", args=(listing_id,)))
+    elif request.POST["action"] == "watchlist":
+        Listing.objects.get(pk=listing_id).watchers.add(request.user)
+        return HttpResponseRedirect(reverse("listing", args=(listing_id,)))
+
+
+def watchlist(request):
+    return render(request, "auctions/watchlist.html", {
+        "listings" : User.objects.get(pk=request.user.id).watchlist.all()
+    })
